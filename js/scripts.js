c78c13c3e85719786df288a8fa627a3c69f27fd3
Matt Eichsteadt <matteichsteadt@yahoo.com>
2016-10-28 13:06:29 -0700

    change variable names, delete one space on html

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,38 @@
-function pingPong(number) {
-  var array = [];
-  if(number > 0) {
-    for(var i = 1; i < number+1; i++) {
+function pingPong(inputNumber) {
+  var outputArray = [];
+  if(inputNumber > 0) {
+    for(var i = 1; i < inputNumber+1; i++) {
       if (i % 15 === 0) {
-        array.push("ping pong");
+        outputArray.push("ping pong");
       }
       else if (i % 5 === 0) {
-        array.push("pong");
+        outputArray.push("pong");
       }
       else if (i % 3 === 0) {
-        array.push("ping");
+        outputArray.push("ping");
       }
       else {
-        array.push(i);
+        outputArray.push(i);
       }
     }
   }
-  else if(number < 0){
-    for(var i = -1; i > number-1; i--) {
+  else if(inputNumber < 0){
+    for(var i = -1; i > inputNumber-1; i--) {
       if (i % 15 === 0) {
-        array.push("ping pong");
+        outputArray.push("ping pong");
       }
       else if (i % 5 === 0) {
-        array.push("pong");
+        outputArray.push("pong");
       }
       else if (i % 3 === 0) {
-        array.push("ping");
+        outputArray.push("ping");
       }
       else {
-        array.push(i);
+        outputArray.push(i);
       }
     }
   }
-  return array;
+  return outputArray;
 }
 
 
