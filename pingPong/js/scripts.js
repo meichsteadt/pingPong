be68ee3ee71be86e811fe0e16cf6cfaf7520125f
Matt Eichsteadt <matteichsteadt@yahoo.com>
2016-10-28 09:51:50 -0700

    take user input and display result

--- pingPong/js/scripts.js
+++ pingPong/js/scripts.js
@@ -34,3 +34,33 @@ function pingPong(number) {
   }
   return array;
 }
+
+
+$(function(){
+  $('form').submit(function(event) {
+    event.preventDefault();
+    $('#outputToUser').children().remove();
+    var userInput = parseInt($('input#userInput').val());
+    if(!userInput || userInput === 0) {
+      $('#outputToUser').append('<li>Please enter a whole, non-zero number</li>');
+    }
+    else {
+      pingPong(userInput).forEach(function(output) {
+        if(output === "ping") {
+          $('#outputToUser').append('<li class="left">' + output + '</li><div class="ball ball-left"></div>');
+        }
+        else if(output === "pong") {
+          $('#outputToUser').append('<li class="right">' + output + '</li><div class="ball ball-right"></div>');
+        }
+        else if(output === "ping pong") {
+          $('#outputToUser').append('<li class="middle">' + output + '</li>');
+        }
+        else {
+          $('#outputToUser').append('<li>' + output + '</li>');
+        }
+        $('#outputToUser').append('<li>|</li>');
+      });
+    }
+    $('input#userInput').val('');
+  });
+});
